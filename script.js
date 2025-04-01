b73619a50e085071adec5e5fe9275672e33e2feb
Thiago <teyextreme@outlook.com>
2025-04-01 15:20:51 -0300

    footer mobile com animação abrir e fechar

--- script.js
+++ script.js
@@ -47,3 +47,14 @@ Array.from(productContainers).forEach(productContainer => {
     productContainer.appendChild(div);
   }
 });
+
+const toggleFooter = (button) => {
+  let footerItem = button.closest(".footer-item");
+  let categories = footerItem.querySelector(".footer-item-categories");
+
+  if (categories.style.maxHeight) {
+    categories.style.maxHeight = null; 
+  } else {
+    categories.style.maxHeight = categories.scrollHeight + "px"; 
+  }
+}

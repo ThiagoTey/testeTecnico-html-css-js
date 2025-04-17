115e7f0b99b0af5935d91eb362de34c6992caef9
Thiago <teyextreme@outlook.com>
2025-04-17 14:56:43 -0300

    search input funcionando no mobile

--- script/search.js
+++ script/search.js
@@ -1,23 +1,28 @@
-const form = document.getElementById('searchForm');
-const input = document.getElementById('searchInput');
-const message = document.getElementById('message');
-
-form.addEventListener('submit', function (e) {
-    e.preventDefault();
-    const value = input.value.trim();
-
-    if (value) {
+function setupSearch(formId, inputId, messageId) {
+    const form = document.getElementById(formId);
+    const input = document.getElementById(inputId);
+    const message = document.getElementById(messageId);
+  
+    // Envio do formulário
+    form.addEventListener('submit', function (e) {
+      e.preventDefault();
+      const value = input.value.trim();
+  
+      if (value) {
         message.classList.add("block");
         message.textContent = `Você buscou por : ${value}`;
-    } else {
+      }
+    });
+  
+    // escondendo o input
+    input.addEventListener('input', function () {
+      const value = input.value.trim();
+  
+      if (!value) {
         message.classList.remove("block");
-    }
-})
-
-input.addEventListener('input', function () {
-    const value = input.value.trim();
-
-    if (!value) {
-        message.classList.remove("block");
-    }
-});
+      }
+    });
+  }
+//Cria o função no formulario do desktop e no formulario mobile
+setupSearch("searchForm", "searchInput", "message");
+setupSearch("searchFormMobile", "searchInputMobile", "messageMobile");
